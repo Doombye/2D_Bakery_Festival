7530295c4e816d126f7a983f473ff431bca2b544
own115 <own115@naver.com>
2022-05-05 18:31:36 +0900

    데이터컨트롤러 제작중

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;               // dataTime 사용하기 위해
+using System.Text;          // 다른 객체로 변환하기위해
+
+public class DataController : Singleton<DataController>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] Text goldText;
+    [SerializeField] Text clickGoldText;
+    [SerializeField] Text goldPerSecText;
+
+
+    private void Update()
+    {
+        goldText.text = string.Format("{0:#,###0}");
+        clickGoldText.text = "Work :" + string.Format("{0:#,###0}" + "Gold");
+        goldPerSecText.text = "WagePerSec : "
+            + string.Format("{0:#,###0}") + "Gold";
+    }
+
+    /*
+    private string[] goldunit = new string[] { "", "만", "억", "경", "해", "자", "양", "가", "구", "간" };
+    private string GetGoldUnit()
+    {
+        // 자를 단위
+        int GoldcutUnit = 4;
+
+        long gold = DataController.Instance.Gold;
+        // 단위마다 자른 골드 저장
+        List<int> numList = new List<int>();
+
+        // 만 단위마다 자르기
+        int CutGold = (int)Mathf.Pow(10, GoldcutUnit);
+
+        do
+        {
+            // 단위로 자른 골드를 리스트에 저장
+            //numList.Add((int)gold % CutGold);
+            // 만단위 줄이기.
+            //gold /= CutGold;
+        }
+        while (gold >= 1);  // 1보다 크면 반복.
+
+        //단위랑 조합될 문자열
+        string UnitGold = "";
+
+        단위당 잘려진 만큼 반복
+        for (int i = 0; i < numList.Count; i++)
+        {
+            UnitGold = numList[i] + goldunit[i] + UnitGold;
+        }
+
+        return UnitGold;
+    }
+
+    
+    private string GetClickGoldUnit()
+    {
+        // 자를 단위
+        int GoldcutUnit = 4;
+
+        long gold = DataController.Instance.ClickGold;
+        // 단위마다 자른 골드 저장
+        List<int> numList = new List<int>();
+
+        // 만 단위마다 자르기
+        int CutGold = (int)Mathf.Pow(10, GoldcutUnit);
+
+        do
+        {
+            // 단위로 자른 골드를 리스트에 저장
+            numList.Add((int)gold % CutGold);
+            // 만단위 줄이기.
+            gold /= CutGold;
+        }
+        while (gold >= 1);  // 1보다 크면 반복.
+
+        // 단위랑 조합될 문자열
+        string UnitGold = "";
+
+        // 단위당 잘려진 만큼 반복
+        for (int i = 0; i < numList.Count; i++)
+        {
+            UnitGold = numList[i] + goldunit[i] + UnitGold;
+        }
+
+        return UnitGold;
+    }
+    */
+}
